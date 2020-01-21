16ae01fe2e94256e23ad20edb0bdd9747c37be42
Evan099 <891837367@qq.com>
2020-01-21 17:18:48 +0800

    部门管理增删改查

--- oa_dao/src/main/java/com/imooc/oa/dao/DepartmentDao.java
+++ oa_dao/src/main/java/com/imooc/oa/dao/DepartmentDao.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository("departmentDao")
-public interface departmentDao {
+public interface DepartmentDao {
 
     void insert(Department department);
     void update(Department department);
